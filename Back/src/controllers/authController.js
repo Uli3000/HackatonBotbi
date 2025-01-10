c63241a4e3d93649d268e3393980ca7a90ae7739
Uli3000 <ulisesgamer281@gmail.com>
2025-01-10 01:14:20 -0600

    estructura backend inicial

---
+++ Back/src/controllers/authController.js
@@ -0,0 +1,73 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const pool = require("../config/db");
+
+const JWT_SECRET = process.env.JWT_SECRET || "123";
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Faltan campos requeridos" });
+  }
+
+  try {
+    // Verificar si el usuario ya existe
+    const [existingUser] = await pool.query(
+      "SELECT * FROM users WHERE username = ?",
+      [username]
+    );
+    if (existingUser.length > 0) {
+      return res.status(400).json({ message: "El usuario ya existe" });
+    }
+
+    // Encriptar contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await pool.query("INSERT INTO users (username, password) VALUES (?, ?)", [
+      username,
+      hashedPassword,
+    ]);
+
+    res.status(201).json({ message: "Usuario registrado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al registrar el usuario" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Faltan campos requeridos" });
+  }
+
+  try {
+    // Verificar si el usuario existe
+    const [rows] = await pool.query("SELECT * FROM users WHERE username = ?", [
+      username,
+    ]);
+    if (rows.length === 0) {
+      return res.status(401).json({ message: "Credenciales incorrectas" });
+    }
+
+    const user = rows[0];
+
+    // Comparar la contraseña ingresada con la almacenada
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Credenciales incorrectas" });
+    }
+
+    // Generar token JWT
+    const token = jwt.sign({ username: user.username }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "Inicio de sesión exitoso", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al iniciar sesión" });
+  }
+};
